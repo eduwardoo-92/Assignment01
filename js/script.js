e75e0a66afda9434a95255f640711224d323aad2
eduwardoo-92 <eduwardoo_92@hotmail.com>
2024-09-25 01:08:38 -0700

    commit

---
+++ js/script.js
@@ -0,0 +1,109 @@
+//STEP 1
+//let someMonth
+//function theMonth()
+//let currentMonth
+//let summerMonth
+//let myLibraryfunction
+
+//STEP 2
+//2.5 numeric literal
+//'JavaScript' string literal 
+//True boolean literal
+//null literal null 
+
+//STEP 3
+//let compExpression = 10 - 4
+//let varExpression = 5 + 5 
+
+//STEP 4
+//strFirstName
+//strLastName
+//strAddress
+//strCity
+//strState
+//intZipCode
+//intYourAge
+//strReferralSource
+//boolMayWeContactYou
+
+//STEP 5
+//let FirstName = 'Eddie'
+//let zipcode = '91910'
+//let city = 'Chula Vista'
+
+//Step 6
+//let dogYears = 3
+//let age = ' years old is the age of my dog'
+//total = dogYears + age
+//Console: '3 years old is the age of my dog'
+
+//STEP 7
+//let boolAndString = true + ' i need to study'
+//alert(boolAndString)     Console: 'true i need to study'
+//let numberAndBool = 5 + true
+//alert(numberAndBool)     Console: 6
+
+//STEP 8 
+/* The error in the string is with the quotes in 'I'm' 
+since we are using single and double quotes, to fix that 
+we need to use ( I\'m ) and this will fix the syntax error */
+
+//STEP 9 
+//let nullVar = null
+//alert(nullVar)          Console: null
+//let yz
+//alert(yz)               Console: undefined
+
+//STEP 10
+//console.log(typeof 'JavaScript')
+//console.log(typeof 5) 
+//console.log(typeof true) 
+//console.log(typeof object)
+//console.log(typeof undefinedNum)
+
+//STEP 11
+//alert("Hello" + "Eddie Ramirez" + ",welcome to the JavaScript class!")
+
+//STEP 12
+//let name = 'Eduardo Ramirez'
+//alert(`Hello ` + name + ` ,welcome to the JavaScript class!`)
+
+//STEP 13
+//let course = 'JavaScript'
+//alert("Hello " + name + "welcome to the " + course + "class!" )
+
+//STEP 14 
+// alert("Hello Zack Ruvalcaba \nWelcome to the JavaScript class!")
+
+//STEP 15 
+//let name = prompt('What is your name?')
+
+//STEP 16 
+//let class = prompt('What is your class?')
+
+//STEP 17
+// x = 10
+// y = 20
+// console.log(x+y)
+
+//STEP 18
+// x = 20
+// console.log(x + 20)
+
+//STEP 19
+// x = 20
+// console.log(x * 5)
+
+//STEP 20 
+// x = 20 % 3
+// console.log(x / 1)
+
+//STEP 21 
+// x = 20
+// y = 30
+// console.log(y > x)
+
+//STEP 22
+// a = true
+// b = false
+// console.log(a == b)
